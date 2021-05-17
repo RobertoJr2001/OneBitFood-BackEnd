4804db574da82aa5731f50a1b5d89d8e6d622cb6
RobertoJr2001 <junionascimento2001@gmail.com>
2021-05-17 20:25:25 -0300

    preparing controllers

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,6 +1,6 @@
 class ProductCategory < ApplicationRecord
   belongs_to :restaurant
-  has_many :products, class_name: "product", foreign_key: "reference_id"
+  has_many :products
 
   validates :title, presence: true
 end
